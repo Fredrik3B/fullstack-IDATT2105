9cd4570a3728f51dd5fbec05bd33c02427669960
oliverwallinhiggins <oliver.w.h@hotmail.com.com>
2026-03-26 12:10:17 +0100

    Add initial project structure with Maven configuration and Spring Boot setup using spring initilizer

---
+++ Backend/src/main/java/no/ntnu/resturant_manager/ResturantManagerApplication.java
@@ -0,0 +1,13 @@
+package no.ntnu.resturant_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResturantManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResturantManagerApplication.class, args);
+	}
+
+}
