6fb5639b956632e7f7e6fc539b2c47ea53eb22da
Christian Petter Remman <cpremman@gmail.com>
2026-03-26 17:08:25 +0100

    Implemented simple routing to view pages

--- frontend/restaurantapp-frontend/src/router/index.js
+++ frontend/restaurantapp-frontend/src/router/index.js
@@ -1,8 +1,39 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import icFoodLayout from '../views/ic-food/icFoodLayout.vue'
+import icFoodDashboard from '../views/ic-food/icFoodDashboard.vue'
+import icAlcoholLayout from '../views/ic-alcohol/icAlcoholLayout.vue'
+import icAlcoholDashboard from '../views/ic-alcohol/icAlcoholDashboard.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      redirect: '/ic-alcohol'
+    },
+    {
+      path: '/ic-food',
+      component: icFoodLayout,
+      children: [
+        {
+          path: '',
+          name: 'ic-food',
+          component: icFoodDashboard
+        }
+      ]
+    },
+    {
+      path: '/ic-alcohol',
+      component: icAlcoholLayout,
+      children: [
+        {
+          path: '',
+          name: 'ic-alcohol',
+          component: icAlcoholDashboard
+        }
+      ]
+    }
+  ],
 })
 
 export default router
