8d40be509bd9ebe6d0721fcb82658816d497f505
oliverwallinhiggins <oliver.w.h@hotmail.com.com>
2026-03-27 15:41:52 +0100

    B to small b

--- backend/src/main/java/edu/ntnu/idatt2105/backend/BackendApplication.java
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/BackendApplication.java
@@ -2,8 +2,21 @@ package edu.ntnu.idatt2105.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+    "edu.ntnu.idatt2105.backend",
+    "no.ntnu.resturant_manager"
+})
+@EntityScan(basePackages = {
+    "edu.ntnu.idatt2105.backend",
+    "no.ntnu.resturant_manager"
+})
+@EnableJpaRepositories(basePackages = {
+    "edu.ntnu.idatt2105.backend",
+    "no.ntnu.resturant_manager"
+})
 public class BackendApplication {
 
   public static void main(String[] args) {
