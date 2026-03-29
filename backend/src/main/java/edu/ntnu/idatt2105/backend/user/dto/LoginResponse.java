06f70e6486e0dfc39470f95a55622b475a91b247
marius <mariuskip@hotmail.com>
2026-03-29 13:53:59 +0200

    refactor: update LoginResponse to use accessToken and modify login flow in auth store

--- backend/src/main/java/edu/ntnu/idatt2105/backend/user/dto/LoginResponse.java
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/user/dto/LoginResponse.java
@@ -1,11 +1,9 @@
 package edu.ntnu.idatt2105.backend.user.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
 
 @AllArgsConstructor
 public class LoginResponse {
   public final String email;
-  public final String jwt;
+  public final String accessToken;
 }
