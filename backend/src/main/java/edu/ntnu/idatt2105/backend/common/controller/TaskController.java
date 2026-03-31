dfa5393d2dfc30fdd15c3d30c58aef408a2c7cae
Christian Petter Remman <cpremman@gmail.com>
2026-03-31 19:38:30 +0200

    fix: update request mapping paths for Checklist, Task, and TemperatureMeasurement controllers

--- backend/src/main/java/edu/ntnu/idatt2105/backend/common/controller/TaskController.java
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/common/controller/TaskController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/api/tasks")
+@RequestMapping("/tasks")
 public class TaskController {
 
 	private final TaskService taskService;
