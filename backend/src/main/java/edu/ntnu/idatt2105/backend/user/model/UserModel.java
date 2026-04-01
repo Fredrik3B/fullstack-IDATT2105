7adc438e35af5b05056cbd1aa295d668bf16c3a1
Fredrik Borbe <fredrik.borbe@gmail.com>
2026-04-01 12:10:22 +0200

    fix: proper foreign key constraint for user and org

--- backend/src/main/java/edu/ntnu/idatt2105/backend/user/model/UserModel.java
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/user/model/UserModel.java
@@ -9,6 +9,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.util.HashSet;
 import java.util.Set;
@@ -26,8 +27,9 @@ public class UserModel {
   @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
 
-  @Column
-  private UUID organizationId;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "organization_id")
+  private OrganizationModel organization;
 
   @Column(nullable = false)
   private String email;
