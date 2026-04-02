7870cbdb2c8bcb408bf0e09bce14d76872710593
marius <mariuskip@hotmail.com>
2026-04-02 16:53:56 +0200

    feat: DTO and Repository class imlemented. Backend structure for document handling should be inplace

--- backend/src/main/java/edu/ntnu/idatt2105/backend/common/repository/DocumentRepository.java
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/common/repository/DocumentRepository.java
@@ -6,14 +6,15 @@ import edu.ntnu.idatt2105.backend.common.model.enums.DocumentModule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface DocumentRepository extends JpaRepository<DocumentModel, Long> {
 
-    List<DocumentModel> findAllByOrganizationId(Long organizationId);
+    List<DocumentModel> findAllByOrganizationId(UUID organizationId);
 
-    List<DocumentModel> findAllByOrganizationIdAndCategory(Long organizationId, DocumentCategory category);
+    List<DocumentModel> findAllByOrganizationIdAndCategory(UUID organizationId, DocumentCategory category);
 
-    List<DocumentModel> findAllByOrganizationIdAndModule(Long organizationId, DocumentModule module);
+    List<DocumentModel> findAllByOrganizationIdAndModule(UUID organizationId, DocumentModule module);
 
-    List<DocumentModel> findAllByOrganizationIdAndCategoryAndModule(Long organizationId, DocumentCategory category, DocumentModule module);
+    List<DocumentModel> findAllByOrganizationIdAndCategoryAndModule(UUID organizationId, DocumentCategory category, DocumentModule module);
 }
