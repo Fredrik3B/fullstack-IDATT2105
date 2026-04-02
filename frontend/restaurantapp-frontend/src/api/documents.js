6c1d7331c952526eafedb064678112dc4f9158b4
marius <mariuskip@hotmail.com>
2026-04-02 17:01:25 +0200

    feat: iadd documents API module and wire up document list view

---
+++ frontend/restaurantapp-frontend/src/api/documents.js
@@ -0,0 +1,27 @@
+import api from './axiosInstance'
+
+export async function fetchDocuments({ category, module } = {}) {
+  const params = {}
+  if (category) params.category = category
+  if (module) params.module = module
+  const { data } = await api.get('/api/documents', { params })
+  return data
+}
+
+export async function uploadDocument(formData) {
+  const { data } = await api.post('/api/documents', formData, {
+    headers: { 'Content-Type': 'multipart/form-data' },
+  })
+  return data
+}
+
+export async function downloadDocument(id) {
+  const response = await api.get(`/api/documents/${id}/download`, {
+    responseType: 'blob',
+  })
+  return response
+}
+
+export async function deleteDocument(id) {
+  await api.delete(`/api/documents/${id}`)
+}
