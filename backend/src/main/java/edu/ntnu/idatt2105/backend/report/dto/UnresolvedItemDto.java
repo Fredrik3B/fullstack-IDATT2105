be8f4bbd234c4c48911d586fdb61de10568f18e1
Fredrik Borbe <fredrik.borbe@gmail.com>
2026-04-03 18:07:57 +0200

    feat: mapper for tasks

--- backend/src/main/java/edu/ntnu/idatt2105/backend/report/dto/UnresolvedItemDto.java
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/report/dto/UnresolvedItemDto.java
@@ -1,12 +1,16 @@
 package edu.ntnu.idatt2105.backend.report.dto;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+
 public class UnresolvedItemDto {
   private String name;
-  private LocalDate notDoneBy;
+  private LocalDateTime notDoneBy;
 }
