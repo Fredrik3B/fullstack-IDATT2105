ebe8a7cf2cce20980afb73cea2259f77c4e8c637
Fredrik Borbe <fredrik.borbe@gmail.com>
2026-04-03 17:37:42 +0200

    refactor: endedAt instead of comletedBy

--- backend/src/main/java/edu/ntnu/idatt2105/backend/common/model/TasksModel.java
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/common/model/TasksModel.java
@@ -8,8 +8,13 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "activated_tasks")
 public class TasksModel extends AuditableEntity {
 
@@ -28,8 +33,8 @@ public class TasksModel extends AuditableEntity {
 	@Column(name = "period_key", nullable = false, length = 16)
 	private String periodKey;
 
-	@Column(name = "completed_at")
-	private LocalDateTime completedAt;
+	@Column(name = "ended_at")
+	private LocalDateTime endedAt;
 
 	@ManyToOne(fetch = FetchType.LAZY, optional = false)
 	@JoinColumn(name = "checklist_id", nullable = false)
@@ -38,68 +43,4 @@ public class TasksModel extends AuditableEntity {
 	@ManyToOne(fetch = FetchType.LAZY, optional = false)
 	@JoinColumn(name = "task_template_id", nullable = false)
 	private TaskTemplate taskTemplate;
-
-	public boolean isCompleted() {
-		return completed;
-	}
-
-	public void setCompleted(boolean completed) {
-		this.completed = completed;
-	}
-
-	public boolean isFlagged() {
-		return flagged;
-	}
-
-	public void setFlagged(boolean flagged) {
-		this.flagged = flagged;
-	}
-
-	public boolean isActive() {
-		return active;
-	}
-
-	public void setActive(boolean active) {
-		this.active = active;
-	}
-
-	public String getMeta() {
-		return meta;
-	}
-
-	public void setMeta(String meta) {
-		this.meta = meta;
-	}
-
-	public String getPeriodKey() {
-		return periodKey;
-	}
-
-	public void setPeriodKey(String periodKey) {
-		this.periodKey = periodKey;
-	}
-
-	public LocalDateTime getCompletedAt() {
-		return completedAt;
-	}
-
-	public void setCompletedAt(LocalDateTime completedAt) {
-		this.completedAt = completedAt;
-	}
-
-	public ChecklistModel getChecklist() {
-		return checklist;
-	}
-
-	public void setChecklist(ChecklistModel checklist) {
-		this.checklist = checklist;
-	}
-
-	public TaskTemplate getTaskTemplate() {
-		return taskTemplate;
-	}
-
-	public void setTaskTemplate(TaskTemplate taskTemplate) {
-		this.taskTemplate = taskTemplate;
-	}
 }
