2242fa6a73f36c8df12599a43dd9c8bb1e154970
Fredrik Borbe <fredrik.borbe@gmail.com>
2026-04-04 12:51:42 +0200

    feat: deviation report based on mattilsynet

---
+++ backend/src/main/java/edu/ntnu/idatt2105/backend/report/dto/DeviationReport.java
@@ -0,0 +1,63 @@
+package edu.ntnu.idatt2105.backend.report.dto;
+
+import edu.ntnu.idatt2105.backend.common.model.enums.DeviationSeverity;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import lombok.Data;
+
+/**
+ * DTO representing a report from the Norwegian food administration.
+ *
+ * @see <a href="https://mattilsynet-xp7prod.enonic.cloud/_/attachment/inline/5578f5d7-af3b-4089-8f9e-57140277918a:24c68bef096002490e3d7b40947240eaa1ed629a/Skjema%20for%20%C3%A5%20registrere%20avvik.pdf">
+ *   Deviantion report pdf</a>
+ */
+@Schema(description = "Request to create a new deviation report")
+@Data
+public class DeviationReport {
+
+  @NotBlank
+  @Schema(description = "Deviation for")
+  private String deviationName;
+
+  @NotNull
+  @Schema(description = "Severity of the deviation", example = "MINOR")
+  private DeviationSeverity severity;
+
+  @NotNull
+  @Schema(description = "Time of when the deviation happened")
+  private LocalDateTime timestamp;
+
+  @NotBlank
+  @Schema(description = "Deviation noticed by")
+  private String noticedBy;
+
+  @NotBlank
+  @Schema(description = "Deviation was reported to")
+  private String reportedTo;
+
+  @NotBlank
+  @Schema(description = "Deviation was handled by")
+  private String processedBy;
+
+  @NotBlank
+  @Schema(description = "Description of what went wrong")
+  private String description;
+
+  @NotBlank
+  @Schema(description = "Actions immediately taken")
+  private String immediateAction;
+
+  @NotBlank
+  @Schema(description = "Believed cause for deviation to happen")
+  private String believedCause;
+
+  @NotBlank
+  @Schema(description = "Measures implemented for non-repeating of deviations")
+  private String correctiveMeasures;
+
+  @NotBlank
+  @Schema(description = "Measures already done")
+  private String correctiveMeasuresDone;
+}
